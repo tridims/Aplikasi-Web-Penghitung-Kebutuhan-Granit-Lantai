19412b051cdcdac806261da30394f2bf0ae9b1c5
Dimas Tri Mustakim <tri.dimas@student.ub.ac.id>
2022-05-27 18:16:49 +0700

    fix halaman show tidak menampilkan gambar secara benar

--- resources/views/granite_tiles/show.blade.php
+++ resources/views/granite_tiles/show.blade.php
@@ -4,7 +4,7 @@
   <div class="mx-4">
     <x-card class="p-10">
       <div class="flex flex-col items-center justify-center text-center">
-        <img class="w-48 mr-6 mb-6" src="{{$listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image.png')}}" alt="" />
+        <img class="w-48 mr-6 mb-6" src="{{$listing->image ? asset('storage/' . $listing->image) : asset('/images/no-image.png')}}" alt="" />
 
         <h3 class="text-2xl mb-2">
           {{$listing->name}}
